e6c789429d3a1151ac3e4ca845fa786ff0396245
chakma-rajeshwar <rajchakma95@gmail.com>
2026-01-17 16:49:20 +0000

    Added public subnet

--- __main__.py
+++ __main__.py
@@ -9,3 +9,16 @@ vpc = aws.ec2.Vpc("my-vpc",
     }
 )
 pulumi.export("vpc_id", vpc.id)
+
+# Create a public subnet
+public_subnet = aws.ec2.Subnet("public-subnet",
+        vpc_id=vpc.id,
+        cidr_block="10.0.1.0/24",
+        availability_zone="ap-southeast-1a",
+        map_public_ip_on_launch=True,
+        tags={
+            "Name": "public_subnet"
+        }
+)
+
+pulumi.export("public_subnet_id", public_subnet.id)
